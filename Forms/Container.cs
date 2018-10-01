1cde00ee1524fe3cab84ecd240ed8d2ac39b1463
ChupUchup <untuk.software@gmail.com>
2018-10-01 13:22:22 +0700

    Edit user interface

--- Forms/Container.cs
+++ Forms/Container.cs
@@ -12,9 +12,88 @@ namespace AplikasiLaporanKegiatanEkstraKurikuler.Forms
 {
     public partial class Container : Form
     {
+        Ekskul FormEkskul;
+        Guru FormGuru;
+        Siswa FormSiswa;
+
         public Container()
         {
             InitializeComponent();
         }
+
+        private void Container_Shown(object sender, EventArgs e)
+        {
+            //var Login = new Login();
+            //Login.Owner = this;
+            //Login.ShowDialog();
+        }
+
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var Print = new Print();
+            Print.Owner = this;
+            Print.ShowDialog();
+        }
+
+        private void loginToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var Login = new Login();
+            Login.Owner = this;
+            Login.ShowDialog();
+        }
+
+        private void logoutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ekskulToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (FormEkskul == null || FormEkskul.IsDisposed)
+            {
+                if (FormGuru != null) FormGuru.Close();
+                if (FormSiswa != null) FormSiswa.Close();
+                FormEkskul = new Ekskul();
+                FormEkskul.MdiParent = this;
+                FormEkskul.Show();
+            }
+        }
+
+        private void guruToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (FormGuru == null || FormGuru.IsDisposed)
+            {
+                if (FormEkskul != null) FormEkskul.Close();
+                if (FormSiswa != null) FormSiswa.Close();
+                FormGuru = new Guru();
+                FormGuru.MdiParent = this;
+                FormGuru.Show();
+            }
+        }
+
+        private void siswaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (FormSiswa == null || FormSiswa.IsDisposed)
+            {
+                if (FormEkskul != null) FormEkskul.Close();
+                if (FormGuru != null) FormGuru.Close();
+                FormSiswa = new Siswa();
+                FormSiswa.MdiParent = this;
+                FormSiswa.Show();
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var About = new About();
+            About.Owner = this;
+            About.ShowDialog();
+        }
+
     }
 }
--- Forms/Guru.Designer.cs
+++ Forms/Guru.Designer.cs
@@ -28,9 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Guru
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Guru";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Guru";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
